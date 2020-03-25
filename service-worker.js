da7139fcf46b837f8669c3465f96fe9acce2fa3c
ada <ada@ada.ada>
2020-03-25 15:02:51 +0000

    rebuild

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/capital/precache-manifest.07a68810d52b78ff9ab206f3aa2b923d.js"
+  "/capital/precache-manifest.d1d0d7a1a2ee8d2fea76aef4e77d401c.js"
 );
 
 self.addEventListener('message', (event) => {
