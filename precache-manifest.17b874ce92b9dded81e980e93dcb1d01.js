199a24f536a77fff4c7c1437d55b1a6a40309b7e
ada <ada@ada.ada>
2020-03-25 13:05:32 +0000

    build zero

---
+++ precache-manifest.17b874ce92b9dded81e980e93dcb1d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116e6e769cda774c623c716b9ea6f938",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd690f0e4927be9cfbf",
+    "url": "/static/css/main.e1a3f3ad.chunk.css"
+  },
+  {
+    "revision": "d3fc20b12f8819318509",
+    "url": "/static/js/2.dc84466b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.dc84466b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd690f0e4927be9cfbf",
+    "url": "/static/js/main.84a82665.chunk.js"
+  },
+  {
+    "revision": "7576d97f31b7ec1d5e4c",
+    "url": "/static/js/runtime-main.54327486.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
