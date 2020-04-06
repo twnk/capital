6c53bc48c4c1903a7a6a4381627a6168e7ed8ede
ada <ada@ada.ada>
2020-04-06 18:39:20 +0100

    oops forgot to git add

---
+++ precache-manifest.5932af153a4894b37f55e4991444939c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d8ca9ccb1980a6733fcfee5f1086a1",
+    "url": "/capital/index.html"
+  },
+  {
+    "revision": "9e807750c31866d8a297",
+    "url": "/capital/static/css/main.e9bddd0b.chunk.css"
+  },
+  {
+    "revision": "6cdfb50dbf65d59869fc",
+    "url": "/capital/static/js/2.1f4e88b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/capital/static/js/2.1f4e88b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e807750c31866d8a297",
+    "url": "/capital/static/js/main.c80f0656.chunk.js"
+  },
+  {
+    "revision": "7e48b36b710f30ea915e",
+    "url": "/capital/static/js/runtime-main.a95397e6.js"
+  }
+]);
